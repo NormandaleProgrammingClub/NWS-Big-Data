10c1bd2340ace39e3c04d58ec874c0b55edbbd8c
mechanic64 <thor2980@yahoo.com>
2017-09-21 10:34:23 -0500

    update 9-21-17

--- datafilter.cpp
+++ datafilter.cpp
@@ -11,10 +11,27 @@
 int main()
 {
 	string file;
+	char delimiter;
+	int columns = 0;
 	
-	cout << "Hello" << endl;
+	
+	cout << "Please input the complete file directory" << endl;
 	cin >> file;
-	cout << file << endl;
+	cout << "Please input the file delimiter" << endl;
+	cin >> delimiter;
+	cout << "Please input the columns, with no spaces, that you would like:" << endl;
+	cin >> columns;
+	//cout << file << " " << delimiter << " " << columns << endl;
+	
+	
+	fstream data;
+	
+	data.open(file,in);
+	
+	
+	
+	
+	
 	
 	return 0;
 }
