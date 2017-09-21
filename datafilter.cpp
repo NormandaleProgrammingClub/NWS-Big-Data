3692e45827645ac4c0bd24b827ed52231bff68d3
mechanic64 <thor2980@yahoo.com>
2017-09-21 10:14:07 -0500

    first commit

---
+++ datafilter.cpp
@@ -0,0 +1,20 @@
+//Data Filter NWS Data Project
+//Normandale Computer Programming Club
+//Written By: Ben Olson
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+	using namespace std;
+	
+int main()
+{
+	string file;
+	
+	cout << "Hello" << endl;
+	cin >> file;
+	cout << file << endl;
+	
+	return 0;
+}
