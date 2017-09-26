e3f554a048211b2e4164a576cf19068c78559645
Andrew Kapsch <AK.Soapy29@gmail.com>
2017-09-25 20:57:54 -0500

    Initial commit

---
+++ FTPDirectory/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace FTPDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the object used to communicate with the server.  
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://ftp.ncdc.noaa.gov/pub/data/noaa/");
+            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+            // This example assumes the FTP site uses anonymous logon.  
+            //request.Credentials = new NetworkCredential("anonymous", "janeDoe@contoso.com");
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            Console.WriteLine(reader.ReadToEnd());
+
+            Console.WriteLine("Directory List Complete, status {0}", response.StatusDescription);
+
+            Console.ReadLine();
+
+            reader.Close();
+            response.Close();
+        }
+    }
+}
