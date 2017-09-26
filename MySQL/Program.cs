9157f05b96af321d7a601c8fb6618aa9072ad4c2
Andrew Kapsch <AK.Soapy29@gmail.com>
2017-09-26 12:12:43 -0500

    Able to insert into table

--- MySQL/Program.cs
+++ MySQL/Program.cs
@@ -18,16 +18,23 @@ namespace MySQL
 
             try
             {
+                Console.WriteLine("Connecting to server...");
                 conn = new MySqlConnection(myConnectionString);
                 conn.Open();
+                Console.WriteLine("Connected");
             }
             catch (MySqlException ex)
             {
+                Console.WriteLine("Error connecting:");
                 Console.WriteLine(ex.Message);
             }
+            Console.WriteLine();
+            Console.WriteLine();
 
             try
             {
+                Console.WriteLine("Pulling data...");
+                Console.WriteLine();
                 string query = "SELECT col0,col1 FROM CPC.Table";
                 MySqlCommand cmd = new MySqlCommand(query, conn);
                 MySqlDataReader reader = cmd.ExecuteReader();
@@ -38,9 +45,31 @@ namespace MySQL
                     string someStringFromColumnOne = reader.GetString(1);
                     Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
                 }
+                reader.Close();
+                Console.WriteLine();
+                Console.WriteLine("Data pulled");
             }
             catch (MySqlException ex)
             {
+                Console.WriteLine("Error pulling data:");
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            try
+            {
+                Console.WriteLine("Inserting data...");
+                MySqlCommand comm = conn.CreateCommand();
+                comm.CommandText = "INSERT INTO CPC.Table(col0,col1) VALUES(@col0, @col1)";
+                comm.Parameters.AddWithValue("@col0", "Myname");
+                comm.Parameters.AddWithValue("@col1", "Myaddress");
+                comm.ExecuteNonQuery();
+                Console.WriteLine("Data inserted");
+            }
+            catch(MySqlException ex)
+            {
+                Console.WriteLine("Error inserting data:");
                 Console.WriteLine(ex.Message);
             }
 
