1431a3514ba4beef41aac0bc1e60354a8c25894d
Andrew Kapsch <AK.Soapy29@gmail.com>
2017-09-25 20:59:58 -0500

    Initial commit - MySQL Database Code

---
+++ MySQL/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace MySQL
+{
+    class Program
+    {
+        static MySqlConnection conn;
+        static string myConnectionString;
+
+        static void Main(string[] args)
+        {
+            myConnectionString = "server=192.168.10.198;uid=NCCCPC;pwd=NCCCPCPassword2017;database=CPC;";
+
+            try
+            {
+                conn = new MySqlConnection(myConnectionString);
+                conn.Open();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                string query = "SELECT col0,col1 FROM Table";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string someStringFromColumnZero = reader.GetString(0);
+                    string someStringFromColumnOne = reader.GetString(1);
+                    Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
