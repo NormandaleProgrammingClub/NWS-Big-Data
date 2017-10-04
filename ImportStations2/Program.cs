20dae0846f85788655af178d0835fbb4f0e4818d
Andrew Kapsch <AK.Soapy29@gmail.com>
2017-10-04 11:52:59 -0500

    Fixed importing

--- ImportStations2/Program.cs
+++ ImportStations2/Program.cs
@@ -35,7 +35,7 @@ namespace ImportStations2
             Console.WriteLine();
 
             Console.WriteLine("Importing stations");
-            FileStream newStream = File.OpenRead(@"C:\Users\aksoa\OneDrive - Normandale\Computer Club\Big Data\wbanmasterlist.psv\wbanmasterlist.psv");
+            FileStream newStream = File.OpenRead(@"C:\Users\aksoa\OneDrive - Normandale\Computer Club\Big Data\Stations WBAN List\wbanmasterlist.psv\wbanmasterlist.psv");
             StreamReader reader = new StreamReader(newStream);
             int count = 0;
 
@@ -45,7 +45,8 @@ namespace ImportStations2
             {
                 String line = reader.ReadLine();
                 //line = String.Format("\"{0}\"", line.Replace("|", "").Replace("\r", "").Replace("\n", ""));
-                String line2 = String.Format("{0}", line.Replace("\"|", "$%&").Replace("\"", "").Replace("|", "").Replace("$%&", "|"));
+                //String line2 = String.Format("{0}", line.Replace("\"|", "$%&").Replace("\"", "").Replace("|", "").Replace("$%&", "|"));
+                String line2 = String.Format("{0}", line.Replace("\r", "").Replace("\n", ""));
 
                 //Console.WriteLine(line2);
 
@@ -64,50 +65,49 @@ namespace ImportStations2
 
                 Console.WriteLine("    " + lines.Length);*/
 
-                /*Console.WriteLine("NCDCSTN_ID: " + line.Substring(0, 8));
-                Console.WriteLine("COOP_ID: " + line.Substring(9, 6));
-                Console.WriteLine("NWSLI_ID: " + line.Substring(16, 8));
-                Console.WriteLine("GHCND_ID: " + line.Substring(25, 15));
-                Console.WriteLine("NAME_COOP_SHORT: " + line.Substring(41, 30));
-                Console.WriteLine("FIPS_COUNTRY_NAME: " + line.Substring(72, 20));
-                Console.WriteLine("STATE_PROV: " + line.Substring(93, 2));
-                Console.WriteLine("COUNTY: " + line.Substring(96, 50));
-                Console.WriteLine("NWS_CLIM_DIV: " + line.Substring(147, 2));
-                Console.WriteLine("NWS_CLIM_DIV_NAME: " + line.Substring(150, 40));
-                Console.WriteLine("LAT_DEC: " + line.Substring(191, 15));
-                Console.WriteLine("LON_DEC: " + line.Substring(207, 15));
-                Console.WriteLine("LAT_LON_PRECISION: " + line.Substring(223, 10));
-                Console.WriteLine("ELEV_GROUND: " + line.Substring(234, 8));
-                Console.WriteLine("ELEV_GROUND_UNIT: " + line.Substring(243, 6));
-                Console.WriteLine("UTC_OFFSET: " + line.Substring(250, 3));
-                Console.WriteLine("NWS_REGION: " + line.Substring(254, 10));
-                Console.WriteLine("NWS_WFO: " + line.Substring(265, 3));
-                Console.WriteLine("COOP_SOD: " + line.Substring(269, 1));
-                Console.WriteLine("COOP_HPD: " + line.Substring(271, 1));*/
+                /*Console.WriteLine("REGION: " + lines[0].Trim('"'));
+                Console.WriteLine("WBAN_ID: " + lines[1].Trim('"'));
+                Console.WriteLine("STATION_NAME: " + lines[2].Trim('"'));
+                Console.WriteLine("STATE_PROVINCE: " + lines[3].Trim('"'));
+                Console.WriteLine("COUNTY: " + lines[4].Trim('"'));
+                Console.WriteLine("COUNTRY: " + lines[5].Trim('"'));
+                Console.WriteLine("EXTENDED_NAME: " + lines[6].Trim('"'));
+                Console.WriteLine("CALL_SIGN: " + lines[7].Trim('"'));
+                Console.WriteLine("STATION_TYPE: " + lines[8].Trim('"'));
+                Console.WriteLine("DATE_ASSIGNED: " + lines[9].Trim('"'));
+                Console.WriteLine("BEGIN_DATE: " + lines[10].Trim('"'));
+                Console.WriteLine("COMMENTS: " + lines[11].Trim('"'));
+                Console.WriteLine("LOCATION: " + lines[12].Trim('"'));
+                Console.WriteLine("ELEV_OTHER: " + lines[13].Trim('"'));
+                Console.WriteLine("ELEV_GROUND: " + lines[14].Trim('"'));
+                Console.WriteLine("ELEV_RUNWAY: " + lines[15].Trim('"'));
+                Console.WriteLine("ELEV_BAROMETRIC: " + lines[16].Trim('"'));
+                Console.WriteLine("ELEV_STATION: " + lines[17].Trim('"'));
+                Console.WriteLine("ELEV_UPPER_AIR: " + lines[18].Trim('"'));*/
 
                 try
                 {
                     MySqlCommand comm = conn.CreateCommand();
                     comm.CommandText = "INSERT INTO CPC.WBAN(REGION, WBAN_ID, STATION_NAME, STATE_PROVINCE, COUNTY, COUNTRY, EXTENDED_NAME, CALL_SIGN, STATION_TYPE, DATE_ASSIGNED, BEGIN_DATE, COMMENTS, LOCATION, ELEV_OTHER, ELEV_GROUND, ELEV_RUNWAY, ELEV_BAROMETRIC, ELEV_STATION, ELEV_UPPER_AIR) VALUES(@col0, @col1, @col2, @col3, @col4, @col5, @col6, @col7, @col8, @col9, @col10, @col11, @col12, @col13, @col14, @col15, @col16, @col17, @col18)";
-                    comm.Parameters.AddWithValue("@col0", lines[0]);
-                    comm.Parameters.AddWithValue("@col1", lines[1]);
-                    comm.Parameters.AddWithValue("@col2", lines[2]);
-                    comm.Parameters.AddWithValue("@col3", lines[3]);
-                    comm.Parameters.AddWithValue("@col4", lines[4]);
-                    comm.Parameters.AddWithValue("@col5", lines[5]);
-                    comm.Parameters.AddWithValue("@col6", lines[6]);
-                    comm.Parameters.AddWithValue("@col7", lines[7]);
-                    comm.Parameters.AddWithValue("@col8", lines[8]);
-                    comm.Parameters.AddWithValue("@col9", lines[9]);
-                    comm.Parameters.AddWithValue("@col10", lines[10]);
-                    comm.Parameters.AddWithValue("@col11", lines[11]);
-                    comm.Parameters.AddWithValue("@col12", lines[12]);
-                    comm.Parameters.AddWithValue("@col13", lines[13]);
-                    comm.Parameters.AddWithValue("@col14", lines[14]);
-                    comm.Parameters.AddWithValue("@col15", lines[15]);
-                    comm.Parameters.AddWithValue("@col16", lines[16]);
-                    comm.Parameters.AddWithValue("@col17", lines[17]);
-                    comm.Parameters.AddWithValue("@col18", lines[18]);
+                    comm.Parameters.AddWithValue("@col0", lines[0].Trim('"'));
+                    comm.Parameters.AddWithValue("@col1", lines[1].Trim('"'));
+                    comm.Parameters.AddWithValue("@col2", lines[2].Trim('"'));
+                    comm.Parameters.AddWithValue("@col3", lines[3].Trim('"'));
+                    comm.Parameters.AddWithValue("@col4", lines[4].Trim('"'));
+                    comm.Parameters.AddWithValue("@col5", lines[5].Trim('"'));
+                    comm.Parameters.AddWithValue("@col6", lines[6].Trim('"'));
+                    comm.Parameters.AddWithValue("@col7", lines[7].Trim('"'));
+                    comm.Parameters.AddWithValue("@col8", lines[8].Trim('"'));
+                    comm.Parameters.AddWithValue("@col9", lines[9].Trim('"'));
+                    comm.Parameters.AddWithValue("@col10", lines[10].Trim('"'));
+                    comm.Parameters.AddWithValue("@col11", lines[11].Trim('"'));
+                    comm.Parameters.AddWithValue("@col12", lines[12].Trim('"'));
+                    comm.Parameters.AddWithValue("@col13", lines[13].Trim('"'));
+                    comm.Parameters.AddWithValue("@col14", lines[14].Trim('"'));
+                    comm.Parameters.AddWithValue("@col15", lines[15].Trim('"'));
+                    comm.Parameters.AddWithValue("@col16", lines[16].Trim('"'));
+                    comm.Parameters.AddWithValue("@col17", lines[17].Trim('"'));
+                    comm.Parameters.AddWithValue("@col18", lines[18].Trim('"'));
                     comm.ExecuteNonQuery();
                 }
                 catch (MySqlException ex)
